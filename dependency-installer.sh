cfdbb9ea0aa4e76e4b1ad437da19447e86f1f0fb
Tomas <tomas.mella-pickersgill@warwick.ac.uk>
2022-01-06 19:17:01 +0100

    Updating dependencies

--- dependency-installer.sh
+++ dependency-installer.sh
@@ -13,24 +13,25 @@ if [[ $REPLY =~ ^[Yy]$ ]]; then
 
   cd $INSTALL_DIR
 
-  if ! command -v nasm &>/dev/null; then
+  if command -v nasm &>/dev/null; then
     echo "Nasm not found will install to $TOOLS_BIN. Installing ..."
 
     if [ ! -d "nasm-2.15.05" ]; then
 
       if [[ $OSTYPE == 'darwin'* ]]; then
-        curl -OL --silent https://www.nasm.us/pub/nasm/releasebuilds/2.15.05/macosx/nasm-2.15.05-macosx.zip
+        wget -cq https://www.nasm.us/pub/nasm/releasebuilds/2.15.05/macosx/nasm-2.15.05-macosx.zip
         unzip -qq nasm-2.15.05-macosx.zip
       else
         # Linux users will have to build from source
-        curl -O --silent https://www.nasm.us/pub/nasm/releasebuilds/2.15.05/nasm-2.15.05.tar.gz
+        wget -cq --silent https://www.nasm.us/pub/nasm/releasebuilds/2.15.05/nasm-2.15.05.tar.gz
         tar xfz nasm-2.15.05.tar.gz
       fi
     fi
 
     cd nasm-2.15.05
     rm -f $TOOLS_BIN/nasm
-    mv nasm ../bin
+    mv nasm $TOOLS_BIN
+    mv ndisasm $TOOLS_BIN
     cd ..
     rm nasm-2.15.05-macosx.zip
     rm -rf nasm-2.15.05
@@ -41,37 +42,37 @@ if [[ $REPLY =~ ^[Yy]$ ]]; then
   GCC_V=
 
   # TODO: should probably add a check for all tools
-  if ! command -v i386-elf-gcc &>/dev/null; then
+  if command -v x86_64-elf-gcc &>/dev/null; then
 
     echo "Compiler and tools not found, will install at $TOOLS_BIN. Installing ..."
 
-    if [[ $OSTYPE == 'darwin'* ]]; then
-      BINUTILS_V=i386-elf-binutils-apple-darwin-x86_64.tar.bz2
-      GCC_V=i386-elf-gcc-apple-darwin-x86_64.tar.bz2
-    else
-      BINUTILS_V=i386-elf-binutils-apple-darwin-x86_64.tar.bz2
-      GCC_V=i386-elf-gcc-apple-darwin-x86_64.tar.bz2
-    fi
+#    if [[ $OSTYPE == 'darwin'* ]]; then
+#      BINUTILS_V=i386-elf-binutils-apple-darwin-x86_64.tar.bz2
+#      GCC_V=i386-elf-gcc-apple-darwin-x86_64.tar.bz2
+#    else
+#      BINUTILS_V=i386-elf-binutils-apple-darwin-x86_64.tar.bz2
+#      GCC_V=i386-elf-gcc-apple-darwin-x86_64.tar.bz2
+#    fi
 
-    if [ ! -d "i386-elf-binutils" ]; then
-      curl -OL --silent https://github.com/nativeos/i386-elf-toolchain/releases/download/preview/$BINUTILS_V
-      tar -xf $BINUTILS_V
+#    if [ ! -d "i386-elf-binutils" ]; then
+#      wget -cq https://github.com/nativeos/i386-elf-toolchain/releases/download/preview/$BINUTILS_V
+#      tar -xf $BINUTILS_V
+#    fi
+#    cd i386-elf-binutils
+##    mv bin/* $TOOLS_BIN/
+##    cd ..
+##    rm -rf i386-elf-binutils
+##    rm $BINUTILS_V
+
+    if [ ! -d "fake" ]; then
+      wget -cq https://github.com/lordmilko/i686-elf-tools/releases/download/7.1.0/x86_64-elf-tools-linux.zip
+      unzip x86_64-elf-tools-linux.zip
     fi
-    cd i386-elf-binutils
+    cd x86_64-elf-tools-linux
     mv bin/* $TOOLS_BIN/
     cd ..
-    rm -rf i386-elf-binutils
-    rm $BINUTILS_V
-
-    if [ ! -d "i386-elf-gcc" ]; then
-      curl -OL --silent https://github.com/nativeos/i386-elf-toolchain/releases/download/preview/$GCC_V
-      tar -xf $GCC_V
-    fi
-    cd i386-elf-gcc
-    mv bin/* $TOOLS_BIN/
-    cd ..
-    rm -rf i386-elf-gcc
-    rm $GCC_V
+#    rm -rf x86_64-elf-tools-linux
+#    rm x86_64-elf-tools-linux.zip
 
   fi
 
