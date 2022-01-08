58308a63087e6472e7026bbab25378c175ff72e8
Tomas <tomas.mella-pickersgill@warwick.ac.uk>
2022-01-08 17:19:40 +0100

    Adding default qemu v to path

--- config.sh
+++ config.sh
@@ -9,7 +9,7 @@ TEMP_BUILD=$(pwd)/temp-build/
 
 # Assuming that it will be run in DCS system,
 # if not then brew or linux package handler will set it
-QEMU_BIN=/local/java/qemu-i386-softmmu/bin/qemu-system-i386
+QEMU_BIN=/local/java/qemu-i386-softmmu/bin
 
 # Create dir for scripts and add to PATH
 mkdir -p "bin"
@@ -51,13 +51,12 @@ if command -v x86_64-elf-ld &>/dev/null; then
   export LD=x86_64-elf-ld
 fi
 
-
-if command -v qemu-system-i386 &>/dev/null; then
-  echo "\n Warning: could not find qemu-system-i386 on the PATH \n"
+if !command -v qemu-system-i386 &>/dev/null; then
+  echo "Warning: could not find qemu-system-i386 on the PATH"
 fi
 
-if command -v nasm &>/dev/null; then
-  echo "\n Warning: could not find 'nasm' in the PATH. Did you run ./dependency-installer.sh? (not on DCS) \n"
+if !command -v nasm &>/dev/null; then
+  echo "Warning: could not find 'nasm' in the PATH. Did you run ./dependency-installer.sh? (not on DCS)"
 fi
 
 # TODO: here we should also check for required dependencies
