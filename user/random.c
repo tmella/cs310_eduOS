eedc75d248e7a8691d3a0750c617802d68869e37
Tomas <tomas.mella-pickersgill@warwick.ac.uk>
2022-01-24 08:54:51 +0000

    Adding back crt0 file

--- user/random.c
+++ user/random.c
@@ -1,7 +1,15 @@
 
+#include "../stdlib/stdlib.h"
 
 int main(int argc, const char *argv[]) {
-  // This should cause an error
-   asm volatile("hlt");
+    // This should cause an error
+    asm volatile ("mov %0, %%eax;"
+                  "int $0x80;"
+    : : "r" (argc)
+    : "eax"
+    );
+
+    asm volatile("hlt");
+
     while(1);
 }
