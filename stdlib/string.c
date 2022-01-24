3020204c9bf400a75baf437c4f1b90959743d379
Tomas <tomas.mella-pickersgill@warwick.ac.uk>
2022-01-24 08:52:16 +0000

    std new string method and sycalls

--- stdlib/string.c
+++ stdlib/string.c
@@ -6,8 +6,15 @@
 
 #include "string.h"
 
+int strlen(const char *str) {
+    int i = 0;
+    while(str[i])
+        i++;
+    return i;
+}
+
 int strcmp(const char *str1, const char *str2) {
-    while (*str1 && (*str1 == *str2)) {
+    while(*str1 && (*str1 == *str2)) {
         str1++;
         str2++;
     }
